92bf3eb47d08c67c693b153f7ad04c4d28619095
Quist <chni215@student.sdu.dk>
2018-10-30 16:22:00 +0100

    New turn function, move forward in progress

--- motor.py
+++ motor.py
@@ -5,17 +5,31 @@ mL = ev3.LargeMotor('outD')
 mR = ev3.LargeMotor('outA')
 
 runSpeed = 300
-turnSpeed = 100
+turnSpeed = 200
 turnDistanceWheel = 130
 
 
-def moveStraigth(direction, errorL, errorR):
+#def moveStraigth(direction, errorL, errorR):
+#    if direction == 'backward':
+#        mL.run_forever(speed_sp=int(errorL*runSpeed))
+#        mR.run_forever(speed_sp=int(errorR*runSpeed))
+#    else:
+#        mL.run_forever(speed_sp=-int(errorL*runSpeed))
+#        mR.run_forever(speed_sp=-int(errorR*runSpeed))
+
+
+def moveStraigth(direction, sensorLeft, sensorRight, TH):
     if direction == 'backward':
         mL.run_forever(speed_sp=int(errorL*runSpeed))
         mR.run_forever(speed_sp=int(errorR*runSpeed))
     else:
-        mL.run_forever(speed_sp=-int(errorL*runSpeed))
-        mR.run_forever(speed_sp=-int(errorR*runSpeed))
+        diff = 1-(abs(sensorLeft-sensorRight)/TH)
+        if sensorLeft > sensorRight:
+            mL.run_forever(speed_sp=-int(runSpeed))
+            mR.run_forever(speed_sp=-int(runSpeed) * diff)
+        elif sensorLeft < sensorRight:
+            mL.run_forever(speed_sp=-int(runSpeed) * diff)
+            mR.run_forever(speed_sp=-int(runSpeed))
 
 
 def moveReal(direction, distance):
@@ -32,27 +46,23 @@ def moveReal(direction, distance):
 
 
 def stopMovement():
-    mL.stop(stop_action='hold')
-    mR.stop(stop_action='hold')
+    mL.stop(stop_action='brake')
+    mR.stop(stop_action='brake')
     mL.run_forever(speed_sp=0)
     mR.run_forever(speed_sp=0)
 
 
-def turnRel(direction):
+def turnRel(direction, turnDistance):
     if direction == "right":
-        mL.run_to_rel_pos(position_sp=-turnDistanceWheel, speed_sp=(turnSpeed), stop_action="hold")
-        mR.run_to_rel_pos(position_sp=turnDistanceWheel, speed_sp=(turnSpeed), stop_action="hold")
-        if mL.state == 'running':
-            return False
-        else:
-            return True
+        mL.run_to_rel_pos(position_sp=-turnDistance, speed_sp=(turnSpeed), stop_action="hold")
+        mR.run_to_rel_pos(position_sp=turnDistance, speed_sp=(turnSpeed), stop_action="hold")
+        mL.wait_while('running')
+        mR.wait_while('running')
     elif direction == "left":
-        mL.run_to_rel_pos(position_sp=turnDistanceWheel, speed_sp=(turnSpeed), stop_action="hold")
-        mR.run_to_rel_pos(position_sp=-turnDistanceWheel, speed_sp=(turnSpeed), stop_action="hold")
-        if mL.state == 'running':
-            return False
-        else:
-            return True
+        mL.run_to_rel_pos(position_sp=turnDistance, speed_sp=(turnSpeed), stop_action="hold")
+        mR.run_to_rel_pos(position_sp=-turnDistance, speed_sp=(turnSpeed), stop_action="hold")
+        mL.wait_while('running')
+        mR.wait_while('running')
 
 def turn(direction):
     if direction == "right":
--- sokobanV3.py
+++ sokobanV3.py
@@ -11,10 +11,29 @@ lightSensorFront = ev3.LightSensor('in4')
 ## 62-69    White
 ## 4-7      Black
 
-THRESHOLD = 65
-STOP_THRESHOLD = 300
+initSteps = 20
 
 ##======================================================================
+
+def initializeSensors():
+
+    frontValue = 0
+    value = 0
+
+    for i in range(initSteps):
+        sensorLeft = lightSensorLeft.value()
+        sensorRight = lightSensorRight.value()
+        sensorFront = lightSensorFront.value()
+
+        value = value + sensorLeft + sensorRight
+        frontValue = frontValue + sensorFront
+
+
+    value = (value/(initSteps*2))
+    frontValue = (frontValue/initSteps)*0.9
+    return (value, frontValue)
+
+
 
 def GoForward():
     done = False
@@ -30,7 +49,8 @@ def GoForward():
         stopMovement()
         done = True
     else:
-        moveStraigth('forward', errorL, errorR)
+        #moveStraigth('forward', errorL, errorR)
+        moveStraigth('forward', sensorLeft, sensorRight, THRESHOLD)
 
     return done
 
@@ -52,13 +72,27 @@ def GoBackward():
 
     return done
 
-#def Turn(direction):
-#    done = turnRel(direction)
-#    if done == True:
-#        stopMovement()
-#        return done
-#    else:
-#        return done
+def TurnRel(direction):
+    turnRel(direction, 70)
+
+def CenterLine():
+    done = False
+    sensorLeft = lightSensorLeft.value()
+    sensorRight = lightSensorRight.value()
+
+    logfile = open("logfile.txt", "a")
+    logfile.write("sensorvalues: L: " + str(sensorLeft) + " R: " + str(sensorRight) + "\n")
+    logfile.close()
+
+    if abs(sensorLeft - sensorRight) < 5:
+        stopMovement()
+        done = True
+    elif sensorLeft > sensorRight:
+        turn("right")
+    elif sensorRight > sensorLeft:
+        turn("left")
+
+    return done
 
 def Turn(direction):
     done = False
@@ -67,12 +101,12 @@ def Turn(direction):
 
     if direction == "right":
         turn(direction)
-        if sensorRight < 7:
+        if sensorLeft - sensorRight > THRESHOLD*0.8:
             stopMovement()
             done = True
     elif direction == "left":
         turn(direction)
-        if sensorLeft < 7:
+        if sensorRight - sensorLeft > THRESHOLD*0.8:
             stopMovement()
             done = True
 
@@ -109,23 +143,31 @@ def Error():
 
 ##======================================================================
 
-state = "idle"
+
+runForever = True
+
+THRESHOLD, STOP_THRESHOLD = initializeSensors()
+
 
 #listOfMovements = ["driveForward"]
-#listOfMovements = ["turnLeft"]
-#listOfMovements = ["driveForward", "wait", "turnLeft", "wait", "driveForward", "wait", "turnLeft", "wait", "driveForward"]
-listOfMovements = ["driveForward", "turnLeft", "driveForward", "turnLeft", "driveForward"]
+#listOfMovements = ["turnLeft", "wait", "turnLeft", "wait", "turnLeft", "wait", "turnLeft", "wait"]
+listOfMovements = ["driveForward", "driveForward", "driveForward", "driveForward", "turnLeft", "turnLeft", "driveForward", "driveForward", "driveForward", "driveForward", "turnRight", "turnRight"]
+#listOfMovements = ["driveForward", "turnLeft", "driveForward", "turnLeft", "driveForward", "turnLeft", "driveForward", "turnLeft", "stop"]
 #listOfMovements = ["driveForward", "driveForward", "driveForward", "stop"]
 #listOfMovements = ["turnLeft", "wait", "turnRight", "wait", "turnLeft", "wait", "turnRight"]
 #listOfMovements = ["driveForward", "turnRight", "driveForward", "turnLeft", "driveForward", "stop"]
 #listOfMovements = ["driveForward", "driveForward", "turnLeft", "driveForward", "driveForward", "turnLeft", "driveForward", "driveForward", "turnLeft", "driveForward", "driveForward", "turnLeft"]
 #listOfMovements = ["turnRight", "deliverCan", "turnLeft", "driveForward"]
+
+state = "idle"
 index = -1
+turnState = "initial"
 
 
 logfile = open("logfile.txt", "w")
 logfile.write("SOKOBAN LOG FILE \n")
 logfile.write("Number of Moves: " + str(len(listOfMovements)) + "\n")
+logfile.write("Threshold: " + str(THRESHOLD) + " Stop Threshold: " + str(STOP_THRESHOLD) + "\n")
 logfile.close()
 
 while len(listOfMovements) >= index:
@@ -142,7 +184,7 @@ while len(listOfMovements) >= index:
             state = listOfMovements[index]
 
         logfile = open("logfile.txt", "a")
-        logfile.write("index: " + str(index) + " movement: " + listOfMovements[index] + " TAKEN!!" + "\n")
+        logfile.write("Length: " + str(len(listOfMovements)) + "   index: " + str(index) + " movement: " + listOfMovements[index] + " TAKEN!!" + "\n")
         logfile.close()
     elif state == "driveForward":
         done = GoForward()
@@ -157,13 +199,25 @@ while len(listOfMovements) >= index:
         if done:
             state = "idle"
     elif state == "turnRight":
-        done = Turn('right')
-        if done:
-            state = "idle"
+        if turnState == "initial":
+            turnDone = Turn("right")
+            if turnDone:
+                turnState = "primary"
+        elif turnState == "primary":
+            done = CenterLine()
+            if done:
+                state = "idle"
+                turnState = "initial"
     elif state == "turnLeft":
-        done = Turn("left")
-        if done:
-            state = "idle"
+        if turnState == "initial":
+            turnDone = Turn("left")
+            if turnDone:
+                turnState = "primary"
+        elif turnState == "primary":
+            done = CenterLine()
+            if done:
+                state = "idle"
+                turnState = "initial"
     elif state == "deliverCan":
         done = deliverCan()
         if done:
@@ -174,3 +228,11 @@ while len(listOfMovements) >= index:
     else:
         Error()
         break
+
+
+    if runForever == True:
+        if index == len(listOfMovements)-1:
+            index = -1
+            logfile = open("logfile.txt", "a")
+            logfile.write("Repeat sequence #############################################################" + "\n")
+            logfile.close()
